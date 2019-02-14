df9c3b4808ab05e707160b3ba9bb52e931d2f5fd
dante1234 <867628247@qq.com>
2019-02-14 10:38:00 +0800

    change to list

--- hw/src/TestMain.java
+++ hw/src/TestMain.java
@@ -2,7 +2,7 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        List<String> list = new ArrayList<>();
 
         System.out.println("ni hao ya wozai zheli22222 ");
         
